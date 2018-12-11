fca7d9ce3b713cb8986ad6085846acd34fea22d4
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-11 16:16:27 -0500

    update db

--- ncr_api/settings.py
+++ ncr_api/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'ncr_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME':'ncr_database',
+       'USER': 'ncr',
+       'PASSWORD': 'ncr',
+       'HOST': 'localhost'
+   }
 }
 
 
