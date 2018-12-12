149843639b415273b6f1f167fa08eb924bd0ddcd
Tyana <tyana.stone@gmail.com>
2018-12-12 15:46:14 -0500

    finished quick pay icons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 import Nav from './Nav';
 import Footer from './Footer';
+import QuickPay from './QuickPay';
 class Home extends Component {
   render() {
     return (
           <Router>
             <div>
-              <Nav />
-                  Home Component
-                <Switch>
+          <Nav />
+          <QuickPay />
+                  
+                {/* <Switch>
                   R
-                </Switch>
-              <Footer />
+                </Switch> */}
+              {/* <Footer /> */}
             </div>
           </Router>
     );
