6191aec75f68f09eaf69a9d4f4703426d1f12baf
Tyana <tyana.stone@gmail.com>
2018-12-13 00:20:28 -0500

    added icons to navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import './assets/css/Nav.css'
-
+import './assets/css/Nav.css';
+import { Link } from "react-router-dom";
+import ncrlogo from '../images/NCR logo.png';
+import silverlogo from '../images/logo-large.png';
+import cart from '../images/cart.png';
+import profile from '../images/db33b0089e.png';
+import search from '../images/search.jpg';
 class Nav extends Component {
   render() {
     return (
         <div class="grid-container">
-          <div class="Logo">Logo</div>
+        <div class="Logo">
+        <Link to='/'><img src={ncrlogo} alt="ncrlogo" height="110px" width="100px"/></Link>
+        </div>
           <div class="Right">
             <div class="Nav">
               <ul class="flexed">
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Products</a></li>
-                <li><a href='#'>Build A Solution</a></li>
-                <li><a href='#'>Support</a></li>
+              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>About</Link></li>
+              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>Products</Link></li>
+              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>Build A Solution</Link></li>
+              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF'}}>Support</Link></li>
               </ul>
             </div>
             <div class="icons">
             <ul class="flexed">
-                <li><a href='#'>Search</a></li>
-                <li><a href='#'>Profile</a></li>
-                <li><a href='#'>Cart</a></li>
+                <li><Link to='/'><img src={search} alt="search" height="50px"/></Link></li>
+                <li><Link to='/'><img src={profile} alt="profile" height="50px"/></Link></li>
+                <li><Link to='/'><img src={cart} alt="cart" height="50px"/></Link></li>
               </ul>
             </div>
             <div class="Silver-Logo centered">
-            Silver logo
+            <Link to='/'><img src={silverlogo} alt="silverlogo" height="110px" width="300px"/></Link>
             </div>
           </div>
         </div>
