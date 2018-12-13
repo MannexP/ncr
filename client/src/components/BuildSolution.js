d1a5f2158498fbcf9679c8bece6e4f85c0d03a43
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-12 19:48:21 -0500

    get solution pagination working

--- client/src/components/BuildSolution.js
+++ client/src/components/BuildSolution.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
+import Customer from './Customer';
+import BusinessType from './BusinessType';
+import Confirmation from './Confirmation';
+import Success from './Success';
 
 class BuildSolution extends Component {
-  render() {
-    return (
-      <div>
-        BuildSolution Component
-      </div>
-    );
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    age: '',
+    city: '',
+    country: ''
+}
+
+nextStep = () => {
+    const { step } = this.state
+    this.setState({
+        step : step + 1
+    })
+}
+
+prevStep = () => {
+    const { step } = this.state
+    this.setState({
+        step : step - 1
+    })
+}
+
+handleChange = input => event => {
+    this.setState({ [input] : event.target.value })
+}
+
+render(){
+    const {step} = this.state;
+    const { firstName, lastName, email, age, city, country } = this.state;
+    const values = { firstName, lastName, email, age, city, country };
+    switch(step) {
+    case 1:
+        return <Customer 
+                nextStep={this.nextStep} 
+                handleChange = {this.handleChange}
+                values={values}
+                />
+    case 2:
+        return <BusinessType 
+                nextStep={this.nextStep}
+                prevStep={this.prevStep}
+                handleChange = {this.handleChange}
+                values={values}
+                />
+    case 3:
+        return <Confirmation
+                nextStep={this.nextStep}
+                prevStep={this.prevStep}
+                values={values}
+                />
+    case 4:
+        return <Success />
+    }
   }
 }
 
