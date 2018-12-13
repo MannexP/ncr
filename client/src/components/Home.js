b0791e43aa8e2b43acb704defef3bdd5ce7d9c82
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-13 14:28:09 -0500

    fix pull master

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
 import Nav from './Nav';
-// import Footer from './Footer';
+import Footer from './Footer';
 import QuickPay from './QuickPay'
 import BuildSolution from './BuildSolution';
 class Home extends Component {
