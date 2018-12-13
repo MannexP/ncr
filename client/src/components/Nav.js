8dead5a15b77f64729f62f76f843b1093c9a15c8
Tyana <tyana.stone@gmail.com>
2018-12-13 12:00:06 -0500

    added search icon & adjusted links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,31 +5,32 @@ import ncrlogo from '../images/NCR logo.png';
 import silverlogo from '../images/logo-large.png';
 import cart from '../images/cart.png';
 import profile from '../images/db33b0089e.png';
-import search from '../images/search.jpg';
+import { Icon } from 'semantic-ui-react';
 class Nav extends Component {
   render() {
     return (
-        <div class="grid-container">
-        <div class="Logo">
+        <div className="grid-container">
+        <div className="Logo">
         <Link to='/'><img src={ncrlogo} alt="ncrlogo" height="110px" width="100px"/></Link>
         </div>
-          <div class="Right">
-            <div class="Nav">
-              <ul class="flexed">
-              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>About</Link></li>
-              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>Products</Link></li>
-              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF' }}>Build A Solution</Link></li>
-              <li><Link to='#' style={{ textDecoration: 'none', color: '#FFFFFF'}}>Support</Link></li>
+          <div className="Right">
+            <div className="Nav">
+              <ul className="flexed">
+              <li><Link to='#' className="titles" style={{ textDecoration: 'none', color: '#FFFFFF' }}>ABOUT</Link></li>
+              <li><Link to='#' className="titles" style={{ textDecoration: 'none', color: '#FFFFFF' }}>PRODUCTS</Link></li>
+              <li><Link to='#' className="titles" style={{ textDecoration: 'none', color: '#FFFFFF' }}>BUILD A SOLUTION</Link></li>
+              <li><Link to='#' className="titles" style={{ textDecoration: 'none', color: '#FFFFFF'}}>SUPPORT</Link></li>
               </ul>
             </div>
-            <div class="icons">
-            <ul class="flexed">
-                <li><Link to='/'><img src={search} alt="search" height="50px"/></Link></li>
+            <div className="icons">
+            <ul className="flexed">
+              {/* <li><Link to='/'><img src={search} alt="search" height="50px"/></Link></li> */}
+              <li><Icon className="search huge"/></li>
                 <li><Link to='/'><img src={profile} alt="profile" height="50px"/></Link></li>
                 <li><Link to='/'><img src={cart} alt="cart" height="50px"/></Link></li>
               </ul>
             </div>
-            <div class="Silver-Logo centered">
+            <div className="Silver-Logo centered">
             <Link to='/'><img src={silverlogo} alt="silverlogo" height="110px" width="300px"/></Link>
             </div>
           </div>
