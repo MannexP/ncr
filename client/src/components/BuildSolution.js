0412e20f23ab1384157ebd8127f6fb4d477d1509
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-12 19:16:09 -0500

    initial build solution

---
+++ client/src/components/BuildSolution.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BuildSolution extends Component {
+  render() {
+    return (
+      <div>
+        BuildSolution Component
+      </div>
+    );
+  }
+}
+
+export default BuildSolution;
