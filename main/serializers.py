cc5b31b534d94b9203eed15f6d791ce9b3b03d5e
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-15 18:58:03 -0500

    get user id

--- main/serializers.py
+++ main/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('id', 'username',)
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
