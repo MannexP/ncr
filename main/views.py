0566c0495f4ce06dd13d34c2fc0e7a3f7b77465e
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-16 19:59:17 -0500

    add permissons to objects

--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .permissions import IsAdminOrReadOnly, IsOwner
 from .serializers import ProfileSerializer, ProductSerializer, ProductTypeSerializer, BillingSerializer, \
     ShoppingCartSerializer, ShippingSerializer, OrderItemSerializer, TrackPackageSerializer, OrderHistorySerializer, \
     UserSerializer, UserSerializerWithToken
@@ -31,45 +32,54 @@ class UserList(APIView):
 
 
 class ProfileView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class ProductView(viewsets.ModelViewSet):
+    # permission_classes = (IsAdminOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductTypeView(viewsets.ModelViewSet):
+    # permission_classes = (IsAdminOrReadOnly,)
     queryset = ProductType.objects.all()
     serializer_class = ProductTypeSerializer
 
 
 class BillingView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = Billing.objects.all()
     serializer_class = BillingSerializer
 
 
 class ShoppingCartView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = ShoppingCart.objects.all()
     serializer_class = ShoppingCartSerializer
 
 
 class ShippingView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = Shipping.objects.all()
     serializer_class = ShippingSerializer
 
 
 class OrderHistoryView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = OrderHistory.objects.all()
     serializer_class = OrderHistorySerializer
 
 
 class OrderItemView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
 
 
 class TrackPackageView(viewsets.ModelViewSet):
+    # permission_classes = (permissions.IsAuthenticated, IsOwner)
     queryset = TrackPackage.objects.all()
     serializer_class = TrackPackageSerializer
