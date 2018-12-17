75a7bcf30ce3dd08106c17609497d19e6c20e80a
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-12-17 17:53:22 -0500

    Added style to products

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ncrquantum from '../images/ncrquantum.png';
-import ncrscanner from '../images/ncrscanner.png';
-import ncrstand from '../images/ncrstand.png';
-import ncrreader from '../images/ncrreader.png';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import ncr1 from '../images/ncr1.png';
+import ncr2 from '../images/ncr2.png';
+import ncr3 from '../images/ncr3.png';
+import ncr4 from '../images/ncr4.png';
+import ncr5 from '../images/ncr5.png';
+import dropdown from '../images/dropdown.png'
+
 
 class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+      }
+    
+      toggle() {
+        this.setState({ collapse: !this.state.collapse });
+      }
     state = {
         product: []
     }
@@ -21,18 +35,19 @@ class Product extends Component {
     render() {
         return (
             <div>
-                {/* <img src={ncrquantum} alt='ncrquantum' />
-                <img src={ncrscanner} alt='ncrscanner' />
-                <img src={ncrstand} alt='ncrstand' />
-                <img src={ncrreader} alt='ncrreader' /> */}
-            
-            
-                {this.state.product.map(product => (
-                    <div key={product.id}>
-                        <img src={product.image_url} alt="" />
-                        <h1>{product.name}</h1>
-                    </div>
-                ))}
+		
+               <img src={ncr1} onClick={this.toggle} alt='ncr1' width='100%'/>
+               <img src={ncr2} onClick={this.toggle} alt='ncr2' width='100%'/>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            <img src={dropdown} alt='dropdown' width='100%'/>
+            </CardBody>
+          </Card>
+        </Collapse>
+               <img src={ncr3} alt='ncr3' width='100%'/>
+               <img src={ncr4} alt='ncr4' width='100%'/>
+               <img src={ncr5} alt='ncr5' width='100%'/>
             </div>
         );
 
