4693d6c3eb86ffde784a787f33160c13a2939fb5
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-12-17 11:18:26 -0500

    Pushing Products

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import ncrquantum from '../images/ncrquantum.png';
+import ncrscanner from '../images/ncrscanner.png';
+import ncrstand from '../images/ncrstand.png';
+import ncrreader from '../images/ncrreader.png';
 
 class Product extends Component {
     state = {
@@ -7,7 +11,7 @@ class Product extends Component {
     }
 
     componentDidMount() {
-        axios.get(`/api/product/`).then(res =>{
+        axios.get(`/api/product/`).then(res => {
             console.log(res.data)
             this.setState({
                 product: res.data
@@ -18,14 +22,21 @@ class Product extends Component {
     render() {
         return (
             <div>
+                {/* <img src={ncrquantum} alt='ncrquantum' />
+                <img src={ncrscanner} alt='ncrscanner' />
+                <img src={ncrstand} alt='ncrstand' />
+                <img src={ncrreader} alt='ncrreader' /> */}
+            
+            
                 {this.state.product.map(product => (
                     <div key={product.id}>
-                    <img src={product.image_url} alt="" />
-                    <h1>{product.name}</h1>
+                        <img src={product.image_url} alt="" />
+                        <h1>{product.name}</h1>
                     </div>
                 ))}
             </div>
         );
+
     }
 }
 
