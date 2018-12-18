34f425fe0847f6d8e3122dd486b17e4461101e2b
Richard Almonte <richard.almonte@bridgesite.io>
2018-12-18 16:12:28 -0500

    WIP styling BillingForm

--- client/src/components/Checkout-Components/BillingForm.js
+++ client/src/components/Checkout-Components/BillingForm.js
@@ -15,142 +15,134 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-  },
-  dense: {
-    marginTop: 16,
-  },
-  menu: {
-    width: 200,
-  },
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	textField: {
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+	},
+	dense: {
+		marginTop: 16,
+	},
+	menu: {
+		width: 200,
+	},
 });
 
 
 
 
 class OutlinedTextFields extends React.Component {
-  state = {
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  };
+	state = {
+		name: 'Cat in the Hat',
+		age: '',
+		multiline: 'Controlled',
+		currency: 'EUR',
+	};
 
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
+	handleChange = name => event => {
+		this.setState({
+			[name]: event.target.value,
+		});
+	};
 
-  render() {
-    const { classes } = this.props;
+	render() {
+		const { classes } = this.props;
 
-      return (
-          
-          
-          <form className={classes.container} noValidate autoComplete="off">
-              <div>Card Type*</div>
-              <img src={visa} alt="visa" height="50px"/>
-              <img src={mastercard} alt="visa" height="50px"/>
-              <img src={amex} alt="visa" height="50px"/>
-              <img src={discover} alt="visa" height="50px"/>
-              <div>
+			return (
+				<div>
+				<div>Card Type*</div>
+				<img src={visa} alt="visa" height="50px"/>
+				<img src={mastercard} alt="visa" height="50px"/>
+				<img src={amex} alt="visa" height="50px"/>
+				<img src={discover} alt="visa" height="50px"/>
+				<div>
+					<form className={classes.container} noValidate autoComplete="off">
+						<div>
+						<TextField required
+							id="outlined-number"
+							label="Card Number"
+							value={this.state.age}
+							onChange={this.handleChange('age')}
+							type="number"
+							className={classes.textField}
+							InputLabelProps={{
+								shrink:require
+							}}
+							margin="normal"
+								variant="outlined"
 
-              <div>
-        <TextField required
-          id="outlined-number"
-          label="Card Number"
-          value={this.state.age}
-          onChange={this.handleChange('age')}
-          type="number"
-          className={classes.textField}
-          InputLabelProps={{
-            shrink:require
-          }}
-          margin="normal"
-                variant="outlined"
-             
-        />
-       </div>
-      <div>
-      <TextField required
-          id="outlined-with-placeholder"
-          label="Name On Card"
-          placeholder="Full Name"
-          className={classes.textField}
-          margin="normal"
-          variant="outlined"
-        />
-         </div> 
-<div>
-     <TextField
-        id="date"
-        label="Expiration Date"
-        type="date"
-        defaultValue="2017-05-24"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-                  </div>
-                  
-     <div>
-        <TextField required
-          id="outlined-number"
-          label="CVV"
-          value={this.state.age}
-          onChange={this.handleChange('age')}
-          type="number"
-          className={classes.textField}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          margin="normal"
-                variant="outlined"
-             
-        />
-       </div>
-     
-     
-       <FormControl component="fieldset">
-        <FormLabel component="legend"></FormLabel>
-        <RadioGroup
-          aria-label="position"
-          name="position"
-          value={this.state.value}
-          onChange={this.handleChange}
-          row
-        >
-        
-          <FormControlLabel
-            value="end"
-            control={<Radio color="primary" />}
-            label="Billing address is same as shipping"
-            labelPlacement="end"
-                /> 
-            
-        </RadioGroup>
-      </FormControl>
-          
-        
-        
-        
-     
-       </div>
-      </form>
-    );
-  }
+						/>
+						</div>
+						<div>
+						<TextField required
+							id="outlined-with-placeholder"
+							label="Name On Card"
+							placeholder="Full Name"
+							className={classes.textField}
+							margin="normal"
+							variant="outlined"
+							/>
+								</div> 
+						<div>
+							<TextField
+								id="date"
+								label="Expiration Date"
+								type="date"
+								defaultValue="2017-05-24"
+								className={classes.textField}
+								InputLabelProps={{
+									shrink: true,
+								}}
+								/>
+						</div>
+
+						<div>
+							<TextField required
+								id="outlined-number"
+								label="CVV"
+								value={this.state.age}
+								onChange={this.handleChange('age')}
+								type="number"
+								className={classes.textField}
+								InputLabelProps={{
+									shrink: true,
+								}}
+								margin="normal"
+									variant="outlined"
+
+							/>
+							</div>
+							<FormControl component="fieldset">
+								<FormLabel component="legend"></FormLabel>
+								<RadioGroup
+									aria-label="position"
+									name="position"
+									value={this.state.value}
+									onChange={this.handleChange}
+									row
+								>
+
+							<FormControlLabel
+								value="end"
+								control={<Radio color="primary" />}
+								label="Billing address is same as shipping"
+								labelPlacement="end"
+									/> 
+
+						</RadioGroup>
+						</FormControl>
+						</form>
+						</div>
+				</div>
+	);
+	}
 }
 
 OutlinedTextFields.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 
