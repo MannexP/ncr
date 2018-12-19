e09bfb22aba7adf36cba8f2b4ceafbf6571f6fe7
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-12-18 19:55:26 -0500

    feature style done

--- client/src/components/Solution-Components/Features.js
+++ client/src/components/Solution-Components/Features.js
@@ -3,7 +3,7 @@ import { Form, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle, faBoxes } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import Solution from './images/solutionImage.png';
 
@@ -19,6 +19,24 @@ const SolutionStyles = styled.div`
 const FeatureStyles = styled.div`
 height: 1000px;
 `
+
+const Header = styled.div`
+display: flex;
+justify-content: center;
+margin-top: 40px;
+`
+const Choices = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`
+const Boxes = styled.div`
+display: flex;
+justify-content: center;
+`
+
+
 class Features extends Component {
   next = (e) => {
     e.preventDefault()
@@ -41,31 +59,42 @@ class Features extends Component {
               <Icon onClick={this.back} aria-hidden="true" className="chevron left big"></Icon>
             </div>
             <div className="grouped fields">
-              <h1>Would you like any of these features?</h1>
-              <p>Table layout  •  Transfer Tabs  •  Happy Hour Pricing  •  Forced Modifiers  •  Takeout or Delivery
-            <br />
-                Advanced Check Splitting  •  House Accounts  •  Multi-Station Kitchen Printer Routing  •  Barista or Kitchen Display</p>
+              <Header>
+                <h1>Would you like any of these features?</h1>
+              </Header>
+              <Choices>
+                <h5>Table layout  •  Transfer Tabs  •  Happy Hour Pricing  •  Forced Modifiers  •  Takeout or Delivery</h5>
+              </Choices>
+          
+              <Choices>
+                <h5>Advanced Check Splitting  •  House Accounts  •  Multi-Station Kitchen Printer Routing  •  Barista or Kitchen Display</h5>
+              </Choices>
+
+
               <div className="field">
+                <Boxes>
+                  <div className="pretty p-icon p-plain p-tada">
 
-                <div className="pretty p-icon p-plain p-tada">
-                  <input type="radio" name="featureAnswer"
-                    value="Yes" onClick={this.onChange} />
-                  <div className="state p-primary-o">
+                    <input type="radio" name="featureAnswer"
+                      value="Yes" onClick={this.onChange} />
+                    <div className="state p-primary-o">
 
-                    <FontAwesomeIcon className='icon' icon={faCheckCircle} style={{ color: 'green' }} />
-                    <label>Yes</label>
+                      <FontAwesomeIcon className='icon' icon={faCheckCircle} style={{ color: 'green' }} />
+                      <label>Yes</label>
+                    </div>
                   </div>
-                </div>
 
-                <div className="pretty p-icon p-plain p-tada">
-                  <input type="radio" name="featureAnswer"
-                    value="No" onClick={this.onChange} />
-                  <div className="state p-primary-o">
+                  <div className="pretty p-icon p-plain p-tada">
+                    <input type="radio" name="featureAnswer"
+                      value="No" onClick={this.onChange} />
+                    <div className="state p-primary-o">
 
-                    <FontAwesomeIcon className='icon' icon={faTimesCircle} style={{ color: 'red' }} />
-                    <label>No</label>
+                      <FontAwesomeIcon className='icon' icon={faTimesCircle} style={{ color: 'red' }} />
+                      <label>No</label>
+                    </div>
                   </div>
-                </div>
+                </Boxes>
+
               </div>
             </div>
             <div>
