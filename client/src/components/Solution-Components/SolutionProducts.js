2d212126f4ac5ac4eef15e4879c5dae3c220c22d
Tyana <tyana.stone@gmail.com>
2018-12-18 20:22:10 -0500

    finished solution products

--- client/src/components/Solution-Components/SolutionProducts.js
+++ client/src/components/Solution-Components/SolutionProducts.js
@@ -4,14 +4,32 @@ import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
-
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import Solution from './images/solutionImage.png';
+import styled from 'styled-components';
+import logo from '../../images/Icon.png';
+import { Icon } from 'semantic-ui-react';
 
 library.add(faTimesCircle)
+library.add(faShoppingCart)
+library.add(faPlusCircle)
 
+const Straight = styled.div`
+display: flex;
+justify-content: space-around;
 
+margin-top: 20px;
+.check{
+    display:flex;
+}
+`
+const CartStyle = styled.div`
 
+    display:flex;
+
+`
 class SolutionProducts extends Component {
   state={
     products: [],
@@ -52,30 +70,43 @@ class SolutionProducts extends Component {
   render() {
     return (
       <div>
-        <div>
+        <img src={Solution} alt="solution image" width="100%" />
+        <Straight>
+          
+        
           {this.state.products.map(product => (
             <div key={product.id}>
-              <img src={product.image_url} alt="" />
-              <h1>{product.name}</h1>
-              <p>{product.description}</p>
-              <h2>price: ${product.price}</h2>
+              <img src={product.image_url} alt="bundles" height="200px" width="200px" style={{borderRadius: '25px'}}/>
+              <h2>{product.name}</h2>
+          
+                <div className="check"><img src={logo} alt="icon" height="20px" /><h4>NCR Silver Commerce</h4></div>
+                <div className="check"><img src={logo} alt="icon" height="20px" /><h4>NCR Console Core</h4></div>
+                <div className="check"><img src={logo} alt="icon" height="20px" /><h4>ipad Air</h4></div>
+                <div className="check"><img src={logo} alt="icon" height="20px" /><h4>Silver Concierge</h4></div>
+                <div className="check"><img src={logo} alt="icon" height="20px" /><h4>NCR Silver Quantum</h4></div>
+               <h2>24/7 Support</h2>
+               <h3> <Icon onClick={this.next} aria-hidden="true" className="chevron right"></Icon> Product Details</h3>
+             
+              <bold><h1>price: ${product.price}</h1></bold>
 
-              <Button color="success" onClick={() => this.handleClick(product)} >Add to cart</Button>
+              <Button color="success" onClick={() => this.handleClick(product)} ><FontAwesomeIcon className= 'icon' icon={faPlusCircle}/> Add to cart</Button>
               </div>
           ))}
-        </div>
+        </Straight>
         <div>
           <div className="row m-t-1">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
               {this.state.cart.length > 0 ?
                 <div>
+                  <hr />
                   <h4>Shopping cart</h4>
-                  <Link to='/checkout'><Button color="primary">Checkout: Grand Total ${this.state.grandTotal}</Button></Link>
+                  <Link to='/checkout'><Button color="primary"><FontAwesomeIcon className= 'icon' icon={faShoppingCart}/> Checkout: Grand Total ${this.state.grandTotal}</Button></Link>
                   <span></span>
                 </div>
                 :
                 null
               }
+              <CartStyle classname="list-group-item">
               {this.state.cart.map(product => (
                 <div className="list-group" key={product.id}>
                   <span>
@@ -84,10 +115,10 @@ class SolutionProducts extends Component {
                         <h5>{product.name}</h5>
                       </div>
                       <div className="list-group-item-text row">
-                        <div className='col-xs-12 col-sm-4 col-md-4 col-lg-4 m-b-1'><img src={product.image_url} alt="" /></div>
+                        <div className='col-xs-12 col-sm-4 col-md-4 col-lg-4 m-b-1'><img src={product.image_url} alt="bundles" height="100px" style={{borderRadius: '25px'}}/></div>
                       </div>
                       <div className='col-xs-12 col-sm-4 col-md-4'>
-                        <p>{product.description}</p>
+                       
                         <p>price: ${product.price}</p>
                       </div>
                       <div className="col-xs-12 col-sm-4 col-md-4 col-lg-3">
@@ -96,7 +127,8 @@ class SolutionProducts extends Component {
                     </div>
                   </span>
                 </div>
-              ))}
+                ))}
+                </CartStyle>
             </div>
           </div>
         </div>
